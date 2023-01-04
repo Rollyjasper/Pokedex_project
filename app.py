e4038d1b7a8d19daab92810e686e7cc393ea0dd1
WillSerby <wserby@forestreet.com>
2023-01-04 11:44:46 +0000

    Template test

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('__name__')
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route('/route<int:route_num>')
+def route_info(route_num):
+    return render_template('route_info.html',route_num=route_num)
