d8444372ba5cb6173a7104e7ca013b6fa4cd6575
WillSerby <wserby@forestreet.com>
2023-01-04 12:56:57 +0000

    creating all needed html files

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ app = Flask('__name__')
 def index():
     return render_template('home.html')
 
+#a route for showing a list of all habitats
+@app.route('/habitats/')
+def habitat_list():
+    return render_template('habitat_list.html')
+
 #route for displaying infomation on a habitat
 @app.route('/habitats/<string:habitat>')
-def route_info(habitat):
+def habitat_info(habitat):
     return render_template('habitat_info.html',habitat=habitat)
+
+#a route for showing a list of all pokemon
+@app.route('/pokemon/')
+def pokemon_list():
+    return render_template('pokemon_list.html')
 
 #route for displaying infomation on a pokémon
 @app.route('/pokemon/<int:dex_num>')
