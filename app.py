f64b71d61602224c5fd46e3cc302e1c85d7522f9
WillSerby <wserby@forestreet.com>
2023-01-04 12:34:10 +0000

    plan+experimentation

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+#imports
 from flask import Flask, render_template
 
+#Create flask object
 app = Flask('__name__')
 
+#route for the homepage
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('home.html')
 
-@app.route('/route<int:route_num>')
-def route_info(route_num):
-    return render_template('route_info.html',route_num=route_num)
+#route for displaying infomation on a habitat
+@app.route('/habitats/<string:habitat>')
+def route_info(habitat):
+    return render_template('habitat_info.html',habitat=habitat)
+
+#route for displaying infomation on a pokémon
+@app.route('/pokemon/<int:dex_num>')
+def pokemon_info(dex_num):
+    return render_template('pokemon_info.html',dex_num=dex_num)
