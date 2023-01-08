9d609a706ff66237e98f6973196a271f20f40c8c
WillSerby <wserby@forestreet.com>
2023-01-08 13:47:21 +0000

    table beautification

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 
 #Create flask object
 app = Flask('__name__')
+
 #load the data from json files
 #we do this here to prevent having to load the full json file on page loads
 with open('data/habitats.json','r') as file:
@@ -16,6 +17,8 @@ with open('data/TMs.json','r') as file:
     tm_data = json.load(file)
 with open('data/Tutors.json','r') as file:
     tutor_data = json.load(file)
+with open('data/type_colours.json','r') as file:
+    type_colours = json.load(file)
 
 #route for the homepage
 @app.route('/')
@@ -53,11 +56,32 @@ def pokemon_list():
 #route for displaying infomation on a pokémon
 @app.route('/pokemon/<int:dex_num>')
 def pokemon_info(dex_num):
+    pkm = pokemon_data[str(dex_num)]
+    pri_col = type_colours[pkm['Type'][0]]['Primary']
+    bgd_col = type_colours[pkm['Type'][0]]['Background']
+    if len(pkm['Type']) == 1:
+        sec_col = type_colours[pkm['Type'][0]]['Secondary']
+    else:
+        sec_col = type_colours[pkm['Type'][1]]['Secondary']
+
     return render_template(
         'pokemon_info.html',
         dex_num=dex_num,
-        pokemon_data=pokemon_data[str(dex_num)],
+        pokemon_data=pkm,
         moves=move_data,
         tm_links=tm_data,
-        tutor_moves=tutor_data
+        tutor_moves=tutor_data,
+        primary_colour=pri_col,
+        seconday_colour=sec_col,
+        background_colour=bgd_col,
+        stat_colours=[
+            "#FF5959",
+            '#F5AC78',
+            '#FAE078',
+            '#9DB7F5',
+            '#A7DB8D',
+            '#FA92B2',
+            bgd_col
+        ],
+        type_colours=type_colours
     )
